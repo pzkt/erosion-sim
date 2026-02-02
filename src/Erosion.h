237347995631c2fad526557211194cafe4b69b89
user <>
2026-02-02 23:52:16 +0100

    changes

--- src/Erosion.h
+++ src/Erosion.h
@@ -4,17 +4,16 @@
 
 struct ErosionParams
 {
-    int mapSize = 256;
-    int numDrops = 150000;
-    int maxLifetime = 40;
+    int mapSize = 512;
+    int numDrops = 600000;
+    int maxLifetime = 30;
     float inertia = 0.3f;
     float sedimentCapacityFactor = 4.0f;
     float minSedimentCapacity = 0.01f;
-    float erodeSpeed = 0.02f;
-    float depositSpeed = 0.4f;
-    float evaporateSpeed = 0.03f;
-    float gravity = 3.0f;
-    int smoothingPasses = 0;
+    float erodeSpeed = 0.3f;
+    float depositSpeed = 0.3f;
+    float evaporateSpeed = 0.01f;
+    float gravity = 4.0f;
     int brushRadius = 3;
     float startSpeed = 1.0f;
     float startWater = 1.0f;
